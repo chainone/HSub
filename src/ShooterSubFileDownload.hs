711fb0dab94da3addfd5442cdb42b8ea2e66adac
YoungChen <chainone@gmail.com>
2016-03-05 23:57:57 +0800

    Added sub idx to the file name as somethings the file names returned from server are duplicate

--- src/ShooterSubFileDownload.hs
+++ src/ShooterSubFileDownload.hs
@@ -12,6 +12,7 @@ import Data.List
 import System.IO
 import Control.Monad
 import Data.Maybe
+import System.FilePath.Posix
 import qualified Data.ByteString as DB
 
 httpsString = "https://"
@@ -19,24 +20,27 @@ httpsString = "https://"
 replaceHttpsWithHttp :: String -> String
 replaceHttpsWithHttp s = case stripPrefix httpsString s of Just xs -> "http://" ++ xs
                                                            Nothing -> s
-                                                           
+
 collectSubFiles :: [SubQueryResult] -> [SubFile]
 collectSubFiles r = r >>= files
 
 getFileName :: Response ty -> Maybe String
 getFileName = stripPrefix "attachment; filename=" <=< findHeader (HdrCustom "Content-Disposition")
 
-saveResponseAsFile :: String -> Response DB.ByteString -> IO ()
-saveResponseAsFile dfn rsp = do
-   let fn = fromMaybe dfn $ getFileName rsp
+addFileIndexToFileName :: Int -> FilePath -> FilePath
+addFileIndexToFileName idx p = dropExtension p ++ "_" ++ show idx ++ takeExtension p
+
+saveResponseAsFile :: String -> Int ->  Response DB.ByteString -> IO ()
+saveResponseAsFile dfn idx rsp = do
+   let fn = fromMaybe dfn $ addFileIndexToFileName idx <$> getFileName rsp
    DB.writeFile fn $ rspBody rsp
    putStrLn $ "Downloaded sub file: " ++ fn
 
-downloadSubFile :: String -> SubFile -> IO ()
-downloadSubFile dfn f =  do
+downloadSubFile :: String -> Int -> SubFile -> IO ()
+downloadSubFile dfn idx f =  do
    responseResult <- (simpleHTTP . defaultGETRequest_ . fromJust . parseURI . replaceHttpsWithHttp .link) f :: IO (Result (Response DB.ByteString))
    let defaultFileName = dfn ++ "." ++ ext f
-   case responseResult of Right rsp -> saveResponseAsFile defaultFileName rsp
+   case responseResult of Right rsp -> saveResponseAsFile defaultFileName idx rsp
                           Left error -> putStrLn $ "Error downloading sub file: " ++ defaultFileName ++ "error: " ++ show error
 
 
@@ -45,4 +49,4 @@ downloadSubFiles dfn qrs = do
          let subfiles = collectSubFiles qrs
          putStrLn $ "Matched " ++ (show . length) subfiles ++ " subtitles on server"
          putStrLn "downloading subtitle files..."
-         mapM_ (downloadSubFile dfn) subfiles
+         mapM_ (uncurry $ downloadSubFile dfn) (zip [1,2..] subfiles)
